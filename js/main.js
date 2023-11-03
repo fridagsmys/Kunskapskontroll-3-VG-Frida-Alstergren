73dbe76146271fab1ce0ce4b1fcf8ef6a4924367
Frida Alstergren <frida@alstergren.com>
2023-11-03 16:51:55 +0100

    clean up

--- js/main.js
+++ js/main.js
@@ -10,31 +10,27 @@ async function fetchData() {
     info.innerHTML = "";
 
     try {
-        let response = await fetch('https://restcountries.com/v3.1/lang/' + language)
+        let response = await fetch('https://restcountries.com/v3.1/lang/' + language);
         let data = await response.json();
-        let countries = data.map(element => element.name.common)
+        let countries = data.map(element => element.name.common);
         countryLinks.innerText = "";
         console.log(countries)
 
         for(const country of countries) {
             const name = document.createElement('a');
-            name.classList.add('link')
-            name.href = "#"
-            name.innerText = country
-            countryLinks.append(name)
-            contents.prepend(countryLinks)
+            name.classList.add('link');
+            name.href = "#";
+            name.innerText = country;
+            countryLinks.append(name);
+            contents.prepend(countryLinks);
 
             name.addEventListener('click', fetchCountry)
-        
-            console.log(country)
-
         }
-        console.log(data)
     } catch(error) {
-        alert(`Please enter a language. Make sure it's spelled correctly.`)
-    }
+        alert(`Please enter a language. Make sure it's spelled correctly.`);
+    };
 
-}
+};
 
 
 async function fetchCountry(e) {
@@ -49,7 +45,9 @@ async function fetchCountry(e) {
         </div>
         <h1>${data[0].name.official}</h1>
         <i>Commonly known as ${data[0].name.common}.</i>
-        <p>The capital of ${data[0].name.common} is ${data[0].capital} and they speak ${Object.values(data[0].languages)}.</p>
+        <h4>${data[0].subregion}</h4>
+        <p>The capital of ${data[0].name.common} is ${data[0].capital}
+        <br>and its people speak ${Object.values(data[0].languages)}.</p>
         `
     } else {
         info.innerHTML = `
@@ -58,13 +56,9 @@ async function fetchCountry(e) {
         </div>
         <h1>${data[0].name.official}</h1>
         <i>Commonly known as ${data[0].name.common}.</i>
-        <p>The capital of ${data[0].name.common} is ${data[0].capital} and they speak ${Object.values(data[0].languages)}.</p>
+        <h4>${data[0].subregion}</h4>
+        <p>The capital of ${data[0].name.common} is ${data[0].capital}
+        <br>and its people speak ${Object.values(data[0].languages)}.</p>
         `
-    }
-
-
-    console.log(data)
-}
-
-
-// ERROR HANTERING & CSS
+    };
+};
